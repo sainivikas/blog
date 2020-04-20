b077fef126c593667f4727ac2438337a339e5241
Vikas Saini <vikassaini480@gmail.com>
2020-04-20 20:47:04 +0530

    appengine and kubernetes files added

---
+++ examples/appengine/app.py
@@ -0,0 +1,6 @@
+from flask import Flask
+flask_app = Flask(__name__)
+
+@flask_app.route('/')
+def hello_world():
+    return 'Hello World'
---
+++ examples/kubernetes/README.md
@@ -0,0 +1,3 @@
+# Kubernetes
+## Run Docker image: 
+`docker run -p 80:80 -it sainivikas/blog-example-flask`
